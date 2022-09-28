9f0c3dbf103fb6aad8f088726a65bf61ee15df03
lalee01 <85132183+lalee01@users.noreply.github.com>
2022-09-28 20:32:04 +0000

    Fixed some bug

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import getHeikinAshi from './chart/heikinAshi'
 import stochRsiStrategy from './strategy/stochRsiStrategy'
 
 
-cron.schedule('*/10 * * * * *', async () => {
+cron.schedule('*/30 * * * *', async () => {
 
     try {
         const klines = await getKlines() ?? []
