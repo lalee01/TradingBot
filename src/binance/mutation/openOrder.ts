f1e71dfc1db5329d92973af5be2860c3ddc6e331
Istvan Greczi <gruczika@gmail.com>
2022-10-04 11:20:34 +0200

    feat: implement openorder

---
+++ src/binance/mutation/openOrder.ts
@@ -0,0 +1,64 @@
+import { BinanceClient } from '../connection'
+
+type Options = {
+  stopLoss: number
+  takeProfit: number
+  orderType: "SELL" | "BUY"
+  availableBalance: number
+  openPrice: number
+}
+
+const LEVERAGE = 10
+const CRYPTO_PAIR = process.env.CRYPTO_PAIR
+
+const openOrder = async ({ stopLoss, takeProfit, orderType, availableBalance, openPrice}: Options) => {
+  await BinanceClient.useServerTime()
+  const quantity = ((availableBalance*LEVERAGE)/openPrice).toFixed(3)
+  await BinanceClient.futuresMarginType( CRYPTO_PAIR, 'ISOLATED' ).then((res:any) => console.log(res))
+  await BinanceClient.futuresLeverage( CRYPTO_PAIR, LEVERAGE ).then((res:any) => console.log(res))
+  await BinanceClient.futuresMarketBuy( CRYPTO_PAIR, quantity ).then((res:any) => console.log(res))
+
+  if(orderType === "SELL") {
+    await BinanceClient.futuresMarketSell( CRYPTO_PAIR, quantity ).then((res:any) => console.log(res))
+    await BinanceClient.futuresOrder( "BUY" , CRYPTO_PAIR, 0 , 0 , {
+      type:"STOP_MARKET" , 
+      stopPrice: stopLoss ,
+      workingType: 'MARK_PRICE' , 
+      priceProtect: true ,
+      closePosition: true,
+      timeInForce: 'GTE_GTC',
+    }).then((res: any) => console.log(res))
+    
+    await BinanceClient.futuresOrder( "BUY" , CRYPTO_PAIR, 0 , 0 , {
+      type:"TAKE_PROFIT_MARKET" , 
+      stopPrice: takeProfit ,
+      workingType: 'MARK_PRICE' , 
+      priceProtect: true ,
+      closePosition: true,
+      timeInForce: 'GTE_GTC',
+    }).then((res: any) => console.log(res))
+    
+  } else if (orderType === "BUY") {
+    await BinanceClient.futuresMarketBuy( CRYPTO_PAIR, quantity ).then((res:any) => console.log(res))
+    await BinanceClient.futuresOrder( "SELL" , CRYPTO_PAIR, 0 , 0 , {
+      type:"STOP_MARKET" , 
+      stopPrice: stopLoss ,
+      workingType: 'MARK_PRICE' , 
+      priceProtect: true ,
+      closePosition: true,
+      timeInForce: 'GTE_GTC',
+    }).then((res:any) => console.log(res))
+
+    await BinanceClient.futuresOrder( "SELL" , CRYPTO_PAIR, 0 , 0 , {
+      type:"TAKE_PROFIT_MARKET" , 
+      stopPrice: takeProfit ,
+      workingType: 'MARK_PRICE' , 
+      priceProtect: true ,
+      closePosition: true,
+      timeInForce: 'GTE_GTC',
+    }).then((res:any) => console.log(res))
+  }
+
+}       
+
+export default openOrder
