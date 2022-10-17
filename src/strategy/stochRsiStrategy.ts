31033bba4ed402dda5e372999f3edf5ca5243baf
lalee01 <85132183+lalee01@users.noreply.github.com>
2022-10-17 23:32:11 +0200

    3.5% SL,5% TP and test tp,sl calculate from env

--- src/strategy/stochRsiStrategy.ts
+++ src/strategy/stochRsiStrategy.ts
@@ -7,13 +7,28 @@ import exponentialMovingAverage from './../indicator/ema'
 import { StochasticRSIOutput } from 'technicalindicators/declarations/momentum/StochasticRSI'
 import { longOrder, shortOrder } from './../binance/sendorder'
 
-const CRYPTO_PAIR = process.env.CRYPTO_PAIR
-
 type Options = {
     srsi: StochasticRSIOutput[]
     klines:Klines[]
     heikinAshi: CandleList & { bearish: boolean[], bullish: boolean[], doji: boolean[]}
 }
+/*
+type Order = {
+    type:string
+    sl:number
+    tp:number
+}
+*/
+const CRYPTO_PAIR = process.env.CRYPTO_PAIR
+const TP = Number(process.env.TP)/100
+const SL = Number(process.env.SL)/100
+
+const orderInfo = {
+    side : "SHORT",
+    sl : 0,
+    tp : 0
+  }
+
 
 const stochRsiStrategy = async ({ srsi ,klines , heikinAshi}: Options) => {
 
@@ -54,24 +69,28 @@ const stochRsiStrategy = async ({ srsi ,klines , heikinAshi}: Options) => {
     const shortTradeTrigger =  isItDoji[isItDoji.length-2+indexOffset[0]] && isItBearish[isItBearish.length-1+indexOffset[0]] && markPrice < lastema && crossedShort.some(even)
     const longTradeTrigger = isItDoji[isItDoji.length-2+indexOffset[0]] && isItBullish[isItBullish.length-1+indexOffset[0]] && markPrice > lastema && crossedLong.some(even)
 
-    const slLong = Number((markPrice * 0.998).toFixed(2))
-    const tpLong = Number((markPrice * 1.01).toFixed(2))
-    const slShort = Number((markPrice * 1.002).toFixed(2))
-    const tpShort = Number((markPrice * 0.99).toFixed(2))
+    const slLong = Number((markPrice * 0.9965).toFixed(2))
+    const tpLong = Number((markPrice * 1.005).toFixed(2))
+    const slShort = Number((markPrice * 1.0035).toFixed(2))
+    const tpShort = Number((markPrice * 0.995).toFixed(2))
+
+    orderInfo.sl = orderInfo.side === "SHORT" ? Number(markPrice*(1+SL)) : Number(markPrice*(1-SL))
+    orderInfo.tp = orderInfo.side === "SHORT" ? Number(markPrice*(1-TP)) : Number(markPrice*(1+TP))
 
     if(shortTradeTrigger){
-       
+        orderInfo.side = "SHORT"
         shortOrder({slShort , tpShort})
         sendTelegramMessage(`Short trade : Mark Price :${markPrice} , SL: ${slShort} , TP: ${tpShort}`)
     }
     
     if(longTradeTrigger){
-    
+        orderInfo.side = "LONG"
         longOrder({slLong , tpLong})
         sendTelegramMessage(`Long trade : Mark Price :${markPrice} , SL: ${slLong} , TP: ${tpLong} `)
     }
 
     console.log("-----------------------------------------------------")
+    console.log(orderInfo)
     console.log(new Date())
     console.log("Doji :" , heikinAshi.doji.slice(-3))
     console.log("Bearish :" , heikinAshi.bearish.slice(-3))
