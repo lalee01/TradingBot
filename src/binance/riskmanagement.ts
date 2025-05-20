c77abe397cafcb43352d2769953405398f5b6dbb
lalee01 <85132183+lalee01@users.noreply.github.com>
2025-05-20 23:34:46 +0200

    Added fee correction multiplier to risk management

--- src/binance/riskmanagement.ts
+++ src/binance/riskmanagement.ts
@@ -1,5 +1,6 @@
 
-const riskMultiplier= Number(process.env.RISK)
+const riskMultiplier= Number(process.env.RISK ?? 1)
+const feeCorrectionForLoss = Number(process.env.FEE_CORRECTION ?? 1)
 
 type settingProps = {
     entryPrice : number
@@ -9,9 +10,9 @@ type settingProps = {
 
 const riskManagement = ({entryPrice,stoplossPrice,availableBalance}:settingProps) =>{
 
-    const riskInPrice = availableBalance * (0.01 * riskMultiplier)
-    const distance = Math.abs(entryPrice - stoplossPrice)
-    const quantity =riskInPrice / distance
+  const riskInPrice = availableBalance * feeCorrectionForLoss * (0.01 * riskMultiplier)
+  const distance = Math.abs(entryPrice - stoplossPrice)
+  const quantity =riskInPrice / distance
 
 return quantity
 }
